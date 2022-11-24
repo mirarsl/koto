aeff30ffac07dcffb9baf777b666cda87693f6ca
mirarsl <batuhanarslan@yasamedya.com>
2022-11-24 16:17:54 +0300

    2.1.2+14

--- lib/main.dart
+++ lib/main.dart
@@ -19,32 +19,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  Uri? link;
-  Future<void> initUniLinks() async {
-    /*StreamSubscription _sub;*/
-    try {
-      final initialLink = await getInitialLink();
-      /*_sub = linkStream.listen((String? link) {
-        print(link);
-      });*/
-      if (initialLink != null) {
-        final PendingDynamicLinkData? dynamicLink = await FirebaseDynamicLinks
-            .instance
-            .getDynamicLink(Uri.parse(initialLink.toString()));
-        if (dynamicLink != null) {
-          link = dynamicLink.link;
-        }
-      }
-    } on FormatException {
-      // Handle exception by warning the user their action did not succeed
-      // return?
-    }
-  }
-
-  await initUniLinks();
-
-  runApp(MyApp(link: link));
-
   OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
   OneSignal.shared.setAppId("eb9eaadf-fdf1-4c8b-ad2f-beed7118c8c3");
   // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
@@ -79,6 +53,42 @@ void main() async {
     // (ie. user taps Allow on the permission prompt in iOS)
     print(changes);
   });
+
+  Uri? link;
+
+  Future<void> linkComing(String initialLink) async {
+    final PendingDynamicLinkData? dynamicLink = await FirebaseDynamicLinks
+        .instance
+        .getDynamicLink(Uri.parse(initialLink.toString()));
+    if (dynamicLink != null) {
+      link = dynamicLink.link;
+      Get.off(() => NewsDet(link.toString()));
+    }
+  }
+
+  Future<void> initUniLinks() async {
+    StreamSubscription _sub;
+    try {
+      final initialLink = await getInitialLink();
+      _sub = linkStream.listen((String? link) async {
+        if (link != null) {
+          linkComing(link);
+        }
+      }, onError: (err) {
+        print(err);
+      });
+      if (initialLink != null) {
+        linkComing(initialLink.toString());
+      }
+    } on FormatException {
+      // Handle exception by warning the user their action did not succeed
+      // return?
+    }
+  }
+
+  await initUniLinks();
+
+  runApp(MyApp(link: link));
 }
 
 class MyApp extends StatelessWidget {
